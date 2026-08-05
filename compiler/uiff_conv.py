e4912170f20874d4bfa94c634b2b88b2ee397931
ozaki <bitter.life.club@gmail.com>
2026-08-05 15:58:24 +0900

    Disptcher登録式に変更

--- compiler/uiff_conv.py
+++ compiler/uiff_conv.py
@@ -2,13 +2,116 @@ import json, argparse, os, sys
 from unittest import case
 from iff import *
 
-# コンバート関数
+# 便利関数
 # *****************************************************************************
-class DispatchTree:
+class Area():
+    x: int
+    y: int
+    w: int
+    h: int
+
+    def get_area_buf(self):
+        area_buf = bytearray()
+        area_buf.extend(self.x.to_bytes(2, byteorder='little'))
+        area_buf.extend(self.y.to_bytes(2, byteorder='little'))
+        area_buf.extend(self.w.to_bytes(2, byteorder='little'))
+        area_buf.extend(self.h.to_bytes(2, byteorder='little'))
+        return area_buf
+
+# コンバート各処理
+def util_get_chunk_buf(chunk_type:int, data: bytes):
+    chunk = bytearray()
+    # chunk_typeを2byteで書き出す
+    chunk.extend(int(chunk_type).to_bytes(2, byteorder='little'))
+    # chunk_sizeを2byteで書き出す
+    chunk.extend(len(data).to_bytes(2, byteorder='little'))
+    # dataをそのまま書き出す
+    chunk.extend(data)
+    return chunk
+
+def util_get_chunk_int(chunk_type:int, data:int):
+    return util_get_chunk_buf(chunk_type, data.to_bytes(2, byteorder='little'))
+
+
+# コンバート
+# *****************************************************************************
+class DispatchBase():
+    # 処理するType名を返す
+    def get_process_type(self) -> str:
+        raise NotImplementedError("get_process_type must be implemented in subclasses")
+
+    # chunkを作成する
+    def get_chunk(self, type_info: TypeDispatcher, props: dict, define_data: dict) -> bytes:
+        raise NotImplementedError("get_chunk must be implemented in subclasses")
+
+class TypeDispatcher():
+    type_id: int
+    subtype_id: int
+
+    # デバッグ表示用
+    type_str: str
+    subtype_str: str
+
+    area: Area
+
+    def __init__(self, props: dict, define_data: dict):
+        # Typeの取得
+        self.type_str = props.get("Type")  # デバッグ用
+        self.type_id = define_data.get("Type").get(self.type_str)
+        if self.type_id is None:
+            print(f"Error: Unknown type found: {self.type_str}")
+            sys.exit(1)
+        props.pop("Type", None)  # Typeをpropsから削除する
+
+        # SubTypeの取得
+        self.subtype_str = str(props.get("SubType"))  # デバッグ用
+        subtype_id = props.get("SubType", 0)  # デフォルト値は0
+        if not isinstance(subtype_id, int):
+            # SubTypeがintでない場合(名前指定)はdefine_dataから取得する 
+            subtype_id = define_data.get("SubType").get(subtype_id)
+            if subtype_id is None:
+                print(f"Error: Unknown subtype_id found: {self.subtype_str}")
+                sys.exit(1)
+        self.subtype_id = subtype_id
+        props.pop("SubType", None)  # SubTypeをpropsから削除する
+
+        # Areaの取得
+        # 後で実装する
+        props.pop("AlignCenterX", None)
+
+        self.area = Area()
+        self.area.x = props.get("X", 0)
+        self.area.y = props.get("Y", 0)
+        self.area.w = props.get("W", 0xffff)
+        self.area.h = props.get("H", 0xffff)
+        props.pop("X", None)
+        props.pop("Y", None)
+        props.pop("W", None)
+        props.pop("H", None)
+
+    def get_chunk(self):
+        type_chunk = bytearray()
+
+        # data部を作成する
+        data = bytearray()
+        data.extend(self.type_id.to_bytes(2, byteorder='little'))
+        data.extend(self.subtype_id.to_bytes(2, byteorder='little'))
+        data.extend(self.area.get_area_buf())
+
+        # chunkの作成
+        type_chunk.extend(util_get_chunk_buf(IFF_TYPE, data))
+
+        return type_chunk
+
+class DispatchTree(DispatchBase):
     def __init__(self, json_data: dict|list, define_data: dict):
+        self.define_data = define_data
+
         self.props = {}
         self.children = []
-        self.define_data = define_data
+
+        self.type_dispatchers = {}
+        self.prop_dispatchers = {}
 
         # json_dataがdictかlistかで処理を分ける
         if isinstance(json_data, list):
@@ -24,50 +127,45 @@ class DispatchTree:
                 else:
                     self.props[key] = value
 
+        # type_dispatchersの登録
+        self.add_type_dispatcher(SelectDispatcher)  # TYPE_SELECT用のディスパッチャを登録する
+
+        # prop_dispatchersの登録
+        self.add_prop_dispatcher(TextDispatcher)  # Text用のディスパッチャを登録する
+        self.add_prop_dispatcher(ScriptDispatcher)  # Script用のディスパッチャを登録する
+        self.add_prop_dispatcher(EventsDispatcher)  # Events用のディスパッチャを登録する
+
+    # Dispatchersの追加
+    # *************************************************************************
+    def add_type_dispatcher(self, dispatcher_class:DispatchBase):
+        self.type_dispatchers[dispatcher_class().get_process_type()] = dispatcher_class
+
+    def add_prop_dispatcher(self, dispatcher_class:DispatchBase):
+        self.prop_dispatchers[dispatcher_class().get_process_type()] = dispatcher_class
+
     # コンバート
     # *************************************************************************
-    # コンバート各処理
-    def get_chunk_buf(self, chunk_type:int, data: bytes):
-        data = bytearray()
-        # chunk_typeを2byteで書き出す
-        data.extend(int(chunk_type).to_bytes(2, byteorder='little'))
-        # chunk_sizeを2byteで書き出す
-        data.extend(len(data).to_bytes(2, byteorder='little'))
-        # dataをそのまま書き出す
-        data.extend(data)
-        return data
-
-    def get_chunk_int(self, chunk_type:int, data:int):
-        return self.get_chunk_buf(chunk_type, data.to_bytes(2, byteorder='little'))
-
-    # コンバートチャンク
+    # ノードを再帰的に処理してchunkデータを作成する
     def get_chunk(self):
         data = bytearray()
 
-        # typeのチェック
-        data.extend(self.check_type())
+        # ノードのtype情報を取得して以降の処理内容を決定する
+        type_info = TypeDispatcher(self.props, self.define_data)
+        data.extend(type_info.get_chunk())  # type情報もchunkとして出力
 
-        # selectのチェック
-        data.extend(self.check_select())
+        # Type対象Dispatchersの処理
+        # 特別なTypeでしか使わないpropsを処理する(ユーザー定義Type用)
+        if type_info.type_str in self.type_dispatchers:
+            data.extend(self.type_dispatchers[type_info.type_str]().get_chunk(type_info, self.props, self.define_data))
 
         # 残りのpropsを順番に書き出す
         for key, value in self.props.items():
-            match key:
-                case "Text":
-                    data.extend(self.get_chunk_buf(IFF_TEXT, value.encode('ascii', 'replace')))
-                case "Script":
-                    pass
-                case "Events":
-                    for event in value:
-                        event_id = self.define_data.get("Event").get(event)
-                        if event_id is None:
-                            print(f"Error: Unknown event found: {event}")
-                            sys.exit(1)
-                        data.extend(self.get_chunk_int(IFF_EVENTS, event_id))
-                case _:
-                    raise ValueError(f"Unknown property '{key}' found in {self.chunk_type_str}:{self.subtype_name}. Ignoring.")
+            if key not in self.prop_dispatchers:
+                raise ValueError(f"Unknown property '{key}' found in {type_info.type_str}:{type_info.subtype_str}. Ignoring.")
+            data.extend(self.prop_dispatchers[key]().get_chunk(type_info, {key: value}, self.define_data))
 
         # 子の処理
+        # -----------------------------------------------------------
         children_buf = bytearray()
         for child in self.children:
             children_buf.extend(child.get_chunk())  # 再帰的に子を処理する
@@ -75,96 +173,18 @@ class DispatchTree:
 
         return data
 
-    # 個別処理
-    # *************************************************************************
-    def check_type(self):
-        # デバッグ用に保存しながらTypeを取得
-        self.chunk_type_str = self.props.get("Type")  # デバッグ用
-        if self.chunk_type_str is None:
-            print("Error: Type is missing in the root node.")
-            sys.exit(1)
-
-        # Typeの取得
-        self.chunk_type = self.define_data.get("Type").get(self.chunk_type_str)
-        if self.chunk_type is None:
-            print(f"Error: Unknown type found: {self.chunk_type_str}")
-            sys.exit(1)
-
-        # SubTypeの取得
-        self.subtype_name = self.props.get("SubType", "None")  # デバッグ用
-        subtype = self.props.get("SubType", 0)  # デフォルト値は0
-        if not isinstance(subtype, int):
-            # SubTypeがintでない場合はdefine_dataから取得する 
-            subtype = self.define_data.get("SubType").get(subtype)
-            if subtype is None:
-                print(f"Error: Unknown subtype found: {self.props.get('SubType')}")
-                sys.exit(1)
-        self.props.pop("SubType", None)  # SubTypeをpropsから削除する
-
-        # Areaの取得
-        # 後で実装する
-        self.props.pop("AlignCenterX", None)
-
-        area_buf = bytearray()
-        area_buf.extend(self.props.get("X", 0).to_bytes(2, byteorder='little'))
-        area_buf.extend(self.props.get("Y", 0).to_bytes(2, byteorder='little'))
-        area_buf.extend(self.props.get("W", 0xffff).to_bytes(2, byteorder='little'))
-        area_buf.extend(self.props.get("H", 0xffff).to_bytes(2, byteorder='little'))
-        self.props.pop("X", None)
-        self.props.pop("Y", None)
-        self.props.pop("W", None)
-        self.props.pop("H", None)
-
-        # data部を作成する
-        data = bytearray()
-        data.extend(self.chunk_type.to_bytes(2, byteorder='little'))
-        data.extend(subtype.to_bytes(2, byteorder='little'))
-        data.extend(area_buf)
-
-        # chunkの作成
-        out = bytearray()
-        out.extend(self.get_chunk_buf(IFF_TYPE, data))
-        self.props.pop("Type", None)  # Typeをpropsから削除する
-
-        return out
-
-    def check_select(self):
-        if self.chunk_type != self.define_data.get("Type").get("TYPE_SELECT"):
-            return bytearray()  # Selectタイプ以外は無視する
-
-        # SelRowsの取得
-        rows_num = self.props.get("SelRows", 32767)  # SelRowsがない場合は32767を使用する
-        self.props.pop("SelRows", None)  # SelRowsをpropsから削除する
-
-        # SelItemsの取得
-        sel_item_buf = bytearray()
-        items = self.props.get("SelItems", [])
-        for item in items:
-            sel_item_buf.extend(self.get_chunk_buf(IFF_SEL_ITEM, item.encode('ascii', 'replace')))
-        self.props.pop("SelItems", None)  # SelItemsをpropsから削除する
-
-        # Select用dataの組み立て
-        data = bytearray()
-        data.extend(self.get_chunk_int(IFF_SELECT, rows_num))
-        data.extend(sel_item_buf)
-
-        select_buf = bytearray()
-        select_buf.extend(self.get_chunk_buf(IFF_SELECT, data))
-        self.props.pop("Select", None)  # Selectをpropsから削除する
-
-        return select_buf
 
     # 出力データ作成
     # *****************************************************************************
     def get_uidata(self):
         # dispatch treeを再帰的に処理してdataを作成する
         data = bytearray()
-        if root.props.get("Type") is None:
+        if self.props.get("Type") is None:
             # rootがListの場合はchildrenが処理対象
-            for child in root.children:
+            for child in self.children:
                 data.extend(child.get_chunk())
         else:
-            data.extend(root.get_chunk())
+            data.extend(self.get_chunk())
 
         # header
         out = bytearray()
@@ -185,11 +205,74 @@ class DispatchTree:
         out = self.get_uidata()
         print(" ".join(f"{byte:02X}" for byte in out))
 
+# 個々のPropの処理
+# *****************************************************************************
+class TextDispatcher(DispatchBase):
+    def get_process_type(self):
+        return "Text"
 
-# jsonからDispatchTreeを作成する
+    def get_chunk(self, type_info: TypeDispatcher, props: dict, define_data: dict):
+        text = props.get(self.get_process_type(), "")
+        return util_get_chunk_buf(IFF_TEXT, text.encode('ascii', 'replace'))
+
+class ScriptDispatcher(DispatchBase):
+    def get_process_type(self):
+        return "Script"
+
+    def get_chunk(self, type_info: TypeDispatcher, props: dict, define_data: dict):
+        script = props.get(self.get_process_type(), "")
+        return bytearray()
+
+class EventsDispatcher(DispatchBase):
+    def get_process_type(self):
+        return "Events"
+
+    def get_chunk(self, type_info: TypeDispatcher, props: dict, define_data: dict):
+        events_buf = bytearray()
+        events = props.get(self.get_process_type(), [])
+        for event in events:
+            event_id = define_data.get("Event").get(event)
+            if event_id is None:
+                print(f"Error: Unknown event found: {event}")
+                sys.exit(1)
+            events_buf.extend(util_get_chunk_int(IFF_EVENTS, event_id))
+        return events_buf
+
+
+# 特別なTypeの処理
+# *****************************************************************************
+# TYPE_SELECTの処理
+class SelectDispatcher(DispatchBase):
+    def get_process_type(self):
+        return "TYPE_SELECT"
+
+    def get_chunk(self, type_info: TypeDispatcher, props: dict, define_data: dict):
+        # SelRowsの取得
+        rows_num = props.get("SelRows", 32767)  # SelRowsがない場合は32767を使用する
+        props.pop("SelRows", None)  # SelRowsをpropsから削除する
+
+        # SelItemsの取得
+        sel_item_buf = bytearray()
+        items = props.get("SelItems", [])
+        for item in items:
+            sel_item_buf.extend(util_get_chunk_buf(IFF_SEL_ITEM, item.encode('ascii', 'replace')))
+        props.pop("SelItems", None)  # SelItemsをpropsから削除する
+
+        # Select用dataの組み立て
+        data = bytearray()
+        data.extend(util_get_chunk_int(IFF_SELECT, rows_num))
+        data.extend(sel_item_buf)
+
+        select_buf = bytearray()
+        select_buf.extend(util_get_chunk_buf(IFF_SELECT, data))
+        props.pop("Select", None)  # Selectをpropsから削除する
+
+        return select_buf
+
+
+# コマンドライン処理
+# *****************************************************************************
 if __name__ == "__main__":
-    # コマンドライン処理
-    # *****************************************************************************
     # 引数を取得して、JSONファイルを読み込む
     # ---------------------------------------------------------
     parser = argparse.ArgumentParser(description='Convert UIFF to JSON')
@@ -211,7 +294,6 @@ if __name__ == "__main__":
         for define_file in args.define:
             with open(define_file, 'r') as f:
                 define_data.update(json.load(f))
-
 
     # 結果出力
     root = DispatchTree(ui_data, define_data)
