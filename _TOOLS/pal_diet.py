d4ab2de3a8153135e9389622979340319a3ac27c
ozaki <bitter.life.club@gmail.com>
2026-09-14 13:39:23 +0900

    パレットの効率化

--- _TOOLS/pal_diet.py
+++ _TOOLS/pal_diet.py
@@ -7,7 +7,8 @@ from PIL import Image
 parser = argparse.ArgumentParser(description="Sort palette image by unused colors")
 parser.add_argument("image_path", help="Path to the palette image")
 parser.add_argument("-o", "--output", required=True, help="Output path for the sorted palette image")  # 必須
-parser.add_argument("-m", "--min-palette", type=int, help="Number of minimum colors in the palette")
+parser.add_argument("-m", "--num-palette", type=int, help="Number of colors in the palette")
+parser.add_argument("-k", "--key-index", help="Transparent palette index")
 parser.add_argument("-p", "--gba-palette", action="store_true", help="Use GBA555 format for palette sorting")
 args = parser.parse_args()
 
@@ -18,6 +19,14 @@ img = Image.open(args.image_path)
 # パレット画像でなければエラー
 if img.mode != "P":
     img = img.convert("P")  # パレット画像に変換
+
+pixels = img.get_flattened_data()
+
+# 左上のピクセルのパレット番号を取得(透過用)
+trans_index = pixels[0]
+if args.key_index is not None:
+    trans_index = int(args.key_index)
+
 
 # パレット操作
 # *****************************************************************************
@@ -30,6 +39,9 @@ if args.gba_palette:
     for i in range(len(rgb_palettes)):
         r, g, b = rgb_palettes[i]
         rgb_palettes[i] = [r|0x07, g|0x07, b|0x07]
+
+# 透過色を取得
+trans_color = tuple(rgb_palettes[trans_index])
 
 
 # 参照パレット作成
@@ -52,26 +64,24 @@ sorted_palette = [color for color in sorted_palette if color is not None]  # Non
 # パレットの拡張
 # -------------------------------------------------------------------
 # パレットが指定数オーバーの場合はエラー
-if args.min_palette is not None:
-    if palette_ref_map is None or len(sorted_palette) > args.min_palette:
-        raise ValueError(f"The palette has {len(sorted_palette)} colors, which exceeds the minimum of {args.min_palette}.")
+if args.num_palette is not None:
+    if palette_ref_map is None or len(sorted_palette) > args.num_palette:
+        raise ValueError(f"The palette has {len(sorted_palette)} colors, which exceeds the specified number of {args.num_palette}.")
 
 # パレットの目標とする長さを決定する
 min_palette_num = 16 if len(sorted_palette) <= 16 else 256
-if args.min_palette is not None:  # 指定があればそれを優先
-    min_palette_num = args.min_palette
-
-# 白と黒をぬいた上で拡張準備
-ext_palette = [color for color in sorted_palette if color != (0, 0, 0) and color != (255, 255, 255)]
+if args.num_palette is not None:  # 指定があればそれを優先
+    min_palette_num = args.num_palette
 
 # パレットを拡張する
+ext_palette = [color for color in sorted_palette]
 if min_palette_num > len(ext_palette):
     remaining_colors = min_palette_num - len(ext_palette)
     ext_palette = remaining_colors * [(0, 0, 0)] + ext_palette
 
-# 0と1が[0,0,0]であれば1に白を埋め込められる
-if ext_palette[0] == (0, 0, 0) and ext_palette[1] == (0, 0, 0):
-    ext_palette[1] = (255, 255, 255)  # 白を埋め込む
+# trans_indexを先頭に持ってくる
+trans_index = ext_palette.index(trans_color)  # trans_indexを再取得
+ext_palette.insert(0, ext_palette.pop(trans_index))
 
 
 # 新しいパレットで、新しいピクセルデータを作る
@@ -81,11 +91,9 @@ if ext_palette[0] == (0, 0, 0) and ext_palette[1] == (0, 0, 0):
 reverse_palette = list(reversed(ext_palette))
 
 # 逆パレットを使って新しいパレット番号に置き換える
-pixels = img.get_flattened_data()
 new_pixels = []
 for index in pixels:
-    # 逆パレットのインデックスを取得
-    new_index = reverse_palette.index(tuple(rgb_palettes[index]))
+    new_index = reverse_palette.index(tuple(rgb_palettes[index]))  # 逆パレットのインデックスを取得
     new_pixels.append(min_palette_num-1 - new_index)  # 逆パレット番号から元に戻す
 
 
